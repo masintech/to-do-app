67d2710c71b1158ae0c7f722e29c9e5d4e3184bc
weijeh <weijeh@avery-design.com.tw>
2021-05-06 18:55:54 +0800

    heroku setting

--- server.js
+++ server.js
@@ -5,13 +5,18 @@ let sanitizeHTML = require('sanitize-html')
 let app = express()
 let db
 
+let port = process.env.PORT
+if (port == null || port == "") {
+  port = 3000
+}
+
 app.use(express.static('public'))
 
 let connectionString = 'mongodb+srv://todoapp:p@ssword@cluster0.r9bhx.mongodb.net/items?retryWrites=true&w=majority'
 mongodb.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true }
   , function (err, client) {
     db = client.db()
-    app.listen(3000)
+    app.listen(port)
   })
 
 app.use(express.json())
